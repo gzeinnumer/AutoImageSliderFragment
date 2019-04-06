dc0da95cdb957872cf094253412dbab11eaeb913
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-04-06 23:22:48 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/autoimagesliderfragment/fragment/HomeFragment.java
@@ -0,0 +1,61 @@
+package com.gzeinnumer.autoimagesliderfragment.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.gzeinnumer.autoimagesliderfragment.R;
+import com.gzeinnumer.autoimagesliderfragment.slider.FragmentSlider;
+import com.gzeinnumer.autoimagesliderfragment.slider.SliderIndicator;
+import com.gzeinnumer.autoimagesliderfragment.slider.SliderPagerAdapter;
+import com.gzeinnumer.autoimagesliderfragment.slider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    View view;
+    private SliderView sliderView;
+    private LinearLayout mLinearLayout;
+    private SliderPagerAdapter mAdapter;
+    private SliderIndicator mIndicator;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        view= inflater.inflate(R.layout.fragment_home, container, false);
+        sliderView = (SliderView) view.findViewById(R.id.sliderView);
+        mLinearLayout = (LinearLayout) view.findViewById(R.id.pagesContainer);
+        setupSlider();
+
+        return view;
+    }
+
+    private void setupSlider() {
+        sliderView.setDurationScroll(800);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-1.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-2.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-3.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.menucool.com/slider/prod/image-slider-4.jpg"));
+
+        mAdapter = new SliderPagerAdapter(getFragmentManager(), fragments);
+        sliderView.setAdapter(mAdapter);
+        mIndicator = new SliderIndicator(getActivity(), mLinearLayout, sliderView, R.drawable.indicator_circle);
+        mIndicator.setPageCount(fragments.size());
+        mIndicator.show();
+    }
+
+}
